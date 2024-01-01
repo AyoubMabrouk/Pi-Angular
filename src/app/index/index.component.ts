f63ce05b26c824d413774481893b7a3350d676b2
Ayoub Mabrouk <73364731+AyoubMabrouk@users.noreply.github.com>
2024-01-01 16:52:54 +0100

    Update index.component.ts

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -14,7 +14,6 @@ export class IndexComponent implements OnInit {
       navbarLinks.classList.toggle('active')
     })
     // ...
-    fetch('https://ayoub-mabrouk-python-flask.onrender.com/hello') .then(response => console.log("hello Site fetched"))
   }
 
 
